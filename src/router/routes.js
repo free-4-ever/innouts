001d12e2b086fae492144065dc07309537324d45
Bardiya Bakhshandeh <yakari1991@gmail.com>
2019-05-10 01:54:58 +0100

    getting started

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,6 +6,13 @@ const routes = [
     children: [
       { path: '', component: () => import('pages/Index.vue') }
     ]
+  },
+  {
+    path: '/players',
+    component: () => import('layouts/ID.vue'),
+    children: [
+      { path: '', component: () => import('pages/Player.vue') }
+    ]
   }
 ]
 
