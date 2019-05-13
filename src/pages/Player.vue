dc69c71df474c6b86bb986043b9eec8833e5d10e
Bardiya Bakhshandeh <yakari1991@gmail.com>
2019-05-13 01:50:29 +0100

    worked on the player ID

--- src/pages/Player.vue
+++ src/pages/Player.vue
@@ -1,9 +1,42 @@
 <template>
   <q-page padding>
-    <q-card inline style="width: 500px">
+    <q-card inline >
       <div class="row">  <!-- use 'row' class to define a container / parent -->
-        <div class="col-4">First column</div>  <!-- children will default to 'col'  -->
-        <div class="col-8">Second column</div>
+        <div class="col-sm-4">
+          <div class="row round-borders">
+            <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+          </div>
+          <div class="row">
+            <q-rating
+              color="orange"
+              class="q-mx-auto q-my-sm"
+              size="1.5rem"
+              v-model="ratingModel"
+              :max="5"
+            />
+          </div>
+         <div class="row">
+            <q-rating
+              color="red"
+              icon="favorite"
+              class="q-mx-auto"
+              size="1.5rem"
+              v-model="ratingModel"
+              :max="5"
+            />
+          </div>
+        </div>  <!-- children will default to 'col'  -->
+        <div class="col-sm-8">
+            <q-table
+               title="ID Card"
+               :data="tableData"
+               :columns="columns"
+               separator="none"
+               row-key="name"
+               hide-bottom
+               hide-header
+            />
+        </div>
       </div>
       <!-- <q-card-media>
         <img src="~assets/quasar-logo-full.svg">
@@ -34,9 +67,74 @@
 <script>
 export default {
   // name: 'PageName'
-  data () {
-    // player: Object
-  },
+  data: () => ({
+    columns: [
+      {
+        name: 'desc',
+        required: true,
+        label: 'Dessert (100g serving)',
+        align: 'left',
+        field: 'name',
+        classes: 'my-class',
+        style: 'width: 200px'
+      },
+      {
+        name: 'desc',
+        required: true,
+        label: 'Dessert (100g serving)',
+        align: 'right',
+        field: 'info',
+        classes: 'my-class',
+        style: 'width: 500px'
+      }
+
+    ],
+    tableData: [
+      {
+        name: 'First Name',
+        calories: 159,
+        fat: 6.0,
+        carbs: 24,
+        protein: 4.0,
+        sodium: 87,
+        calcium: '14%',
+        iron: '1%'
+      },
+      {
+        name: 'Last Name',
+        calories: 159,
+        fat: 6.0,
+        carbs: 24,
+        protein: 4.0,
+        sodium: 87,
+        calcium: '14%',
+        iron: '1%'
+      },
+      {
+        name: 'Nationality',
+        calories: 159,
+        fat: 6.0,
+        carbs: 24,
+        protein: 4.0,
+        sodium: 87,
+        calcium: '14%',
+        iron: '1%'
+      },
+      {
+        name: 'Birth',
+        calories: 159,
+        fat: 6.0,
+        carbs: 24,
+        protein: 4.0,
+        sodium: 87,
+        calcium: '14%',
+        iron: '1%'
+      }
+
+    ],
+    ratingModel: 1,
+    player: null
+  }),
 
   mounted: function () {
     this.$axios.get('http://innouts.test/api/players/1')
